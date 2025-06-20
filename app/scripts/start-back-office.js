2bb85701081796cb162d78cde7837b7538032d22
TURPINJonathan <turpin.j@hotmail.fr>
2025-06-20 11:50:19 +0200

    default url for bo

--- app/scripts/start-back-office.js
+++ app/scripts/start-back-office.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 const backOffice = spawn('npm', ['run', 'start'], { cwd: './back-office', stdio: 'inherit' });
 
 backOffice.on('spawn', () => {
-  console.log('🟢 Back-office dev server running at http://localhost:4200');
+  console.log('🟢 Back-office dev server running at http://localhost:4200/akwaytenpo');
 });
 backOffice.on('error', (err) => {
 	console.error('❌ Failed to start back-office dev server:', err);
