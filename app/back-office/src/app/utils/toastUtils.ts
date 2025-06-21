cd56ef25b7641ef6ae377cad599949ffe88b6ebc
TURPINJonathan <turpin.j@hotmail.fr>
2025-06-21 13:19:32 +0200

    add defaut title on toast with translation

--- app/back-office/src/app/utils/toastUtils.ts
+++ app/back-office/src/app/utils/toastUtils.ts
@@ -1,24 +1,44 @@
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { ToastrService, IndividualConfig } from 'ngx-toastr';
 
 @Injectable({ providedIn: 'root' })
 export class ToastUtils {
 
-  constructor(private toastr: ToastrService) {}
+  constructor(
+    private toastr: ToastrService,
+    private translate: TranslateService
+  ) {}
 
-  success(message: string, title = 'Succès', config?: Partial<IndividualConfig>) {
-    this.toastr.success(message, title, { ...config });
+  success(message: string, title?: string, config?: Partial<IndividualConfig>) {
+    this.toastr.success(
+      message,
+      title ?? this.translate.instant('TOAST.TITLE.SUCCESS'),
+      { ...config }
+    );
   }
 
-  error(message: string, title = 'Erreur', config?: Partial<IndividualConfig>) {
-    this.toastr.error(message, title, { ...config });
+  error(message: string, title?: string, config?: Partial<IndividualConfig>) {
+    this.toastr.error(
+      message,
+      title ?? this.translate.instant('TOAST.TITLE.ERROR'),
+      { ...config }
+    );
   }
 
-  info(message: string, title = 'Info', config?: Partial<IndividualConfig>) {
-    this.toastr.info(message, title, { ...config });
+  info(message: string, title?: string, config?: Partial<IndividualConfig>) {
+    this.toastr.info(
+      message,
+      title ?? this.translate.instant('TOAST.TITLE.INFO'),
+      { ...config }
+    );
   }
 
-  warning(message: string, title = 'Attention', config?: Partial<IndividualConfig>) {
-    this.toastr.warning(message, title, { ...config });
+  warning(message: string, title?: string, config?: Partial<IndividualConfig>) {
+    this.toastr.warning(
+      message,
+      title ?? this.translate.instant('TOAST.TITLE.WARNING'),
+      { ...config }
+    );
   }
 }
