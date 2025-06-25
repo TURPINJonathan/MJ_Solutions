16a147d1bb70a65ab1c48a44d0fac29e308e9b2c
TURPINJonathan <turpin.j@hotmail.fr>
2025-06-25 15:22:09 +0200

    add compagny route

--- app/back-office/src/app/app.routes.ts
+++ app/back-office/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '#guards/auth-guard';
 import { DashboardPage } from '#pages/dashboard/dashboard';
 import { LoginPage } from '#pages/login/login';
 import { Routes } from '@angular/router';
+import { CompagnyPage } from '#pages/compagny/compagny';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     canActivateChild: [AuthGuard],
     children: [
       { path: 'dashboard', component: DashboardPage },
+			{ path: 'compagny', component: CompagnyPage},
       { path: '', redirectTo: 'login', pathMatch: 'full' }
     ]
   },
