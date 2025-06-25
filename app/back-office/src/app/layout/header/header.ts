6e72986a35d50ece2285a919bd09e5a6d8650a98
TURPINJonathan <turpin.j@hotmail.fr>
2025-06-25 15:21:49 +0200

    rework nav

--- app/back-office/src/app/layout/header/header.ts
+++ app/back-office/src/app/layout/header/header.ts
@@ -1,23 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  standalone: true,
 	imports: [
+		CommonModule,
+		RouterModule,
 		TranslateModule,
 	]
 })
 export class Header {
   menuOpen = false;
+	activeSubMenu: string | null = null;
 	MJSLogo = 'assets/pictures/MJS_logo.png';
+
+	menus = [
+		{
+			label: 'DASHBOARD.TITLE',
+			route: '/dashboard'
+		},
+    {
+      label: 'PROJECTS.TITLE',
+      route: '/dashboard',
+    },
+    {
+      label: 'SETTINGS.TITLE',
+      subMenu: [
+        { label: 'COMPAGNY.TITLE.PLURAL', route: '/compagny' },
+      ]
+    },
+	]
+
+	get activeSubMenuItems() {
+		return this.menus.find(m => m.label === this.activeSubMenu)?.subMenu ?? [];
+	}
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    if (!this.menuOpen) this.activeSubMenu = null;
   }
 
   closeMenu() {
     this.menuOpen = false;
+    this.activeSubMenu = null;
+  }
+
+  openSubMenu(menuLabel: string, event: Event) {
+    event.preventDefault();
+    this.activeSubMenu = menuLabel;
+  }
+
+  closeSubMenu(event: Event) {
+    event.preventDefault();
+    this.activeSubMenu = null;
   }
 }
