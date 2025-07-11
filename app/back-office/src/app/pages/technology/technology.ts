bf033a8ddfcf89cbce333639a89019a34f44644f
TURPIN Jonathan <turpin.j@hotmail.fr>
2025-07-11 19:33:34 +0200

    show technologies from api in table

--- app/back-office/src/app/pages/technology/technology.ts
+++ app/back-office/src/app/pages/technology/technology.ts
@@ -1,3 +1,6 @@
+import { FileService } from '#BO/services/common/file.service';
+import { TechnologyService } from '#BO/services/technology/technology.service';
+import { ToastUtils } from '#BO/utils/toastUtils';
 import { ButtonComponent } from '#common/ui/button/button';
 import { DialogComponent } from '#common/ui/dialog/dialog';
 import { FormComponent } from '#common/ui/form/form';
@@ -6,7 +9,7 @@ import { TableComponent } from '#common/ui/table/table';
 import { Technology } from '#SModels/technology.model';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-technology',
@@ -28,10 +31,37 @@ export class TechnologyPage {
 	columns = [
 		{ key: 'logo', label: 'COMMON.LOGO' },
 		{ key: 'name', label: 'COMMON.NAME' },
-		{ key: 'type', label: 'TECHNOLOGY.TYPE' },
+		{ key: 'types', label: 'TECHNOLOGY.TYPE' },
 		{ key: 'proficiency', label: 'TECHNOLOGY.PROFICIENCY' },
 		{ key: 'isFavorite', label: 'COMMON.IS_FAVORITE' },
 	]
+
+	constructor(
+		private readonly technologyService: TechnologyService,
+		private readonly fileService: FileService,
+		public translate: TranslateService,
+		private toast: ToastUtils
+	) {
+		this.loadTechnologies();
+	}
+
+	loadTechnologies() {
+		this.technologyService.getAllTechnologies().subscribe({
+			next: (technologies: Technology[]) => {
+				this.technologies = technologies
+
+				if (this.technologies.length === 0) {
+					this.toast.info(this.translate.instant('TOAST.TECHNOLOGY.EMPTY_LIST'), this.translate.instant('TOAST.TITLE.INFO'));
+				} else {
+					this.toast.success(this.translate.instant('TOAST.TECHNOLOGY.SUCCESS_LIST'), this.translate.instant('TOAST.TITLE.SUCCESS'));
+				}
+			},
+			error: (error) => {
+        this.toast.error(this.translate.instant('TOAST.TECHNOLOGY.ERROR_LIST'), this.translate.instant('TOAST.TITLE.ERROR'));
+        console.error('Error loading technologies:', error);
+			}
+		});
+	}
 
 	openCreateTechnologyModal() {
 		console.log('Open create technology modal');
